791ff69e39d3fe5ebcf496f9acc25a290dc6d9d1
Vladislav Dankevich <vielendanke1991@gmail.com>
2021-04-15 14:01:29 +0600

    Pop and Push doubly linked list

--- structures/doubly_linked_list/doublylinkedlist.go
+++ structures/doubly_linked_list/doublylinkedlist.go
@@ -1,9 +1,18 @@
 package main
 
+import (
+	"fmt"
+	"strconv"
+)
+
 type Node struct {
 	prev *Node
 	next *Node
 	val  int
+}
+
+func (n *Node) String() string {
+	return strconv.Itoa(n.val)
 }
 
 type DoublyLinkedList struct {
@@ -14,4 +23,45 @@ type DoublyLinkedList struct {
 
 func NewDoublyLinkedList() *DoublyLinkedList {
 	return &DoublyLinkedList{}
+}
+
+func (dl *DoublyLinkedList) String() string {
+	var arr []int
+	curr := dl.head
+	for curr != nil {
+		arr = append(arr, curr.val)
+		curr = curr.next
+	}
+	return fmt.Sprintf("%v", arr)
+}
+
+func (dl *DoublyLinkedList) Push(val int) {
+	n := &Node{val: val}
+	if dl.head == nil {
+		dl.head = n
+		dl.tail = n
+	} else {
+		dl.tail.next = n
+		n.prev = dl.tail
+		dl.tail = n
+	}
+	dl.length++
+}
+
+func (dl *DoublyLinkedList) Pop() (*Node, bool) {
+	var remove *Node
+	if dl.head == nil {
+		return nil, false
+	}
+	remove = dl.tail
+	if dl.length == 1 {
+		dl.head = nil
+		dl.tail = nil
+	} else {
+		dl.tail = remove.prev
+		dl.tail.next = nil
+		remove.prev = nil
+	}
+	dl.length--
+	return remove, true
 }
--- structures/doubly_linked_list/runner.go
+++ structures/doubly_linked_list/runner.go
@@ -1,5 +1,23 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	dl := &DoublyLinkedList{}
+
+	dl.Push(13)
+
+	fmt.Println(dl.String())
+
+	dl.Push(15)
+
+	fmt.Println(dl.String())
+
+	dl.Push(17)
+
+	fmt.Println(dl.String())
+
+	dl.Pop()
+
+	fmt.Println(dl.String())
 }
